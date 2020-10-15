64823b7890762ea20a5e57200d906419bff650cf
SteffenKoehler <steffen-koehler1@gmx.de>
2020-10-15 15:42:00 +0200

    ✨ let in all run to get the github token

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const clear = require('clear');
 const figlet = require('figlet');
 
 const files = require('./lib/files');
+const github = require('./lib/github');
+const inquirer = require('./lib/inquirer');
 
 clear();
 
 console.log(chalk.yellow(figlet.textSync('Ginit', { horizontalLayout: 'full' })));
 
-if (files.directoryExists('.git')) {
+if (files.directoryExists('.git2')) {
   console.log(chalk.red('Already a Git repository!'));
   process.exit();
 }
+
+const run = async () => {
+  let token = github.getStoredGithubToken();
+  if (!token) {
+      token = await github.getPersonalAccesToken();
+  }
+
+  console.log(token);
+};
+
+run();
